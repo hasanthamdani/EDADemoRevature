d9bf316a3bcc4de88fab947275168ae35099e9a1
hasanthamdani <hasanthamdani01@gmail.com>
2025-02-27 15:14:58 -0500

    Created directory for demos

---
+++ EDAdemoBackEnd/src/main/java/com/example/EDAdemoBackEnd/EdAdemoBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.EDAdemoBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdAdemoBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdAdemoBackEndApplication.class, args);
+	}
+
+}
---
+++ EDAdemoBackEnd/src/test/java/com/example/EDAdemoBackEnd/EdAdemoBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EDAdemoBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdAdemoBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
